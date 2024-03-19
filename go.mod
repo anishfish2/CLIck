b12062fed6dfaf1a25e213d367ae54218fe5917e
Anish Karthik <89824626+anishfish2@users.noreply.github.com>
2024-03-19 15:41:48 -0500

    mpv working

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module CLIck
 
 go 1.22.1
 
-require github.com/joho/godotenv v1.5.1 // indirect
+require (
+	github.com/ebitengine/purego v0.5.1 // indirect
+	github.com/gen2brain/go-mpv v0.2.2 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	golang.org/x/sys v0.7.0 // indirect
+)
--- main.go
+++ main.go
@@ -10,12 +10,14 @@ import (
 	"io"
 
 	"github.com/joho/godotenv"
+	"github.com/gen2brain/go-mpv"
 )
 
 func main() {
 
 	query := flag.String("query", "Code broken :( y?", "The error you are facing.")
 	ask := flag.Bool("ask", false, "Whether to ask ChatGPT for help.")
+	video_url := flag.String("video_url", "", "The URL of the video that was suggested by ChatGPT.")
 
 	flag.Parse()
 
@@ -29,6 +31,71 @@ func main() {
 			return
 		}
 		
+
+		m := mpv.New()
+		defer m.TerminateDestroy()
+
+		_ = m.RequestLogMessages("info")
+		_ = m.ObserveProperty(0, "pause", mpv.FormatFlag)
+
+		_ = m.SetPropertyString("input-default-bindings", "yes")
+		_ = m.SetOptionString("input-vo-keyboard", "yes")
+		_ = m.SetOption("osc", mpv.FormatFlag, true)
+
+		err := m.Initialize()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		err = m.Command([]string{"loadfile", os.Args[1]})
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+	loop:
+		for {
+			e := m.WaitEvent(10000)
+
+			switch e.EventID {
+			case mpv.EventPropertyChange:
+				prop := e.Property()
+				value := prop.Data.(int)
+				fmt.Println("property:", prop.Name, value)
+			case mpv.EventFileLoaded:
+				p, err := m.GetProperty("media-title", mpv.FormatString)
+				if err != nil {
+					fmt.Println("error:", err)
+				}
+				fmt.Println("title:", p.(string))
+			case mpv.EventLogMsg:
+				msg := e.LogMessage()
+				fmt.Println("message:", msg.Text)
+			case mpv.EventStart:
+				sf := e.StartFile()
+				fmt.Println("start:", sf.EntryID)
+			case mpv.EventEnd:
+				ef := e.EndFile()
+				fmt.Println("end:", ef.EntryID, ef.Reason)
+				if ef.Reason == mpv.EndFileEOF {
+					break loop
+				} else if ef.Reason == mpv.EndFileError {
+					fmt.Println("error:", ef.Error)
+				}
+			case mpv.EventShutdown:
+				fmt.Println("shutdown:", e.EventID)
+				break loop
+			default:
+				fmt.Println("event:", e.EventID)
+			}
+
+			if e.Error != nil {
+				fmt.Println("error:", e.Error)
+			}
+		}
+
+
 		if *ask {
 			
 			url := "https://api.openai.com/v1/chat/completions"
