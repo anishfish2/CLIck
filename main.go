52a5a728a0cd45b32376f6d8c3daf8a679f6a88b
Anish Karthik <89824626+anishfish2@users.noreply.github.com>
2024-03-20 23:49:16 -0500

    Update before fixing libmpv.dylib issue

--- main.go
+++ main.go
@@ -48,7 +48,7 @@ func main() {
 			os.Exit(1)
 		}
 
-		err = m.Command([]string{"loadfile", os.Args[1]})
+		err = m.Command([]string{"loadfile", *video_url})
 		if err != nil {
 			fmt.Println(err)
 			os.Exit(1)
