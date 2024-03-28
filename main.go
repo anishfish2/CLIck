e54ad4aaace9d0c0c8a0fffaf4696905d35aebce
Anish Karthik <89824626+anishfish2@users.noreply.github.com>
2024-03-27 23:55:42 -0500

    MVP Working

--- main.go
+++ main.go
@@ -5,18 +5,22 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
+	"io"
+	"log"
 	"net/http"
 	"os"
-	"io"
 	"os/exec"
+
 	"github.com/joho/godotenv"
+	"google.golang.org/api/googleapi/transport"
+	"google.golang.org/api/youtube/v3"
 )
 
 func main() {
 
 	query := flag.String("query", "Code broken :( y?", "The error you are facing.")
-	ask := flag.Bool("ask", false, "Whether to ask ChatGPT for help.")
-	display := flag.Bool("display", true, "Whether to display the video")
+	//answer := flag.Bool("ask", true, "Whether to ask ChatGPT for help.")
+	video := flag.Bool("video", false, "Whether to display the video")
 	video_topic := ""
 
 	flag.Parse()
@@ -26,9 +30,10 @@ func main() {
 		fmt.Println("Error loading .env file", load_env)
 		return
 	}
-	
-	if *ask {
+
 		
+	if *video {
+
 		url := "https://api.openai.com/v1/chat/completions"
 
 		body := map[string]interface{}{
@@ -42,41 +47,27 @@ func main() {
 		}
 
 		jsonBody, err := json.Marshal(body)
-		if err != nil {
-			fmt.Println("Error encoding JSON:", err)
-			return
-		}
 
 		req, _ := http.NewRequest("POST", url, bytes.NewBuffer(jsonBody))
 
 		req.Header = http.Header{
-			"Content-Type": {"application/json"},
+			"Content-Type":  {"application/json"},
 			"Authorization": {"Bearer " + os.Getenv("OPENAI_KEY")},
 		}
 
 		client := &http.Client{}
 
 		res, err := client.Do(req)
-
-		if err != nil {
-			panic(err)
-		}
-
 		defer res.Body.Close()
-		
+
 		resBody, err := io.ReadAll(res.Body)
 
-
 		if err != nil {
-			fmt.Printf("impossible to read response: %s", err)
+			fmt.Printf("Impossible to read OPENAI response: %s", err)
 		}
 
 		var responseBody map[string]interface{}
 		err = json.Unmarshal(resBody, &responseBody)
-		if err != nil {
-			fmt.Printf("Error decoding JSON response: %s", err)
-			return
-		}
 
 		choices := responseBody["choices"].([]interface{})
 		if len(choices) > 0 {
@@ -88,16 +79,40 @@ func main() {
 		fmt.Printf("Search Query: %s", video_topic)
 
 		//Youtube API search query -> get top result url
-		
 
-		if *display {
-			cmd := exec.Command("mpv", "https://www.youtube.com/watch?v=4W6PszUFPsM")
 
-			err := cmd.Run()
-			if err != nil {
-				fmt.Println("Error playing video:", err)
-				return
-			}
+
+		var developerKey = os.Getenv("YOUTUBE_KEY")
+
+		client2 := &http.Client{
+			Transport: &transport.APIKey{Key: developerKey},
 		}
+		service, err := youtube.New(client2)
+
+		if err != nil {
+			log.Fatalf("Error creating new YouTube client: %v", err)
+		}
+
+		call := service.Search.List([]string{}).Q(video_topic).MaxResults(1)
+		
+		response, err := call.Do()
+		video_id := response.Items[0].Id.VideoId
+		fmt.Printf(video_id)
+
+		videoURL := "https://www.youtube.com/watch?v=" + video_id
+		fmt.Printf(videoURL)
+
+		cmd := exec.Command("mpv", videoURL)
+
+		cmd.Run()
+
 	}
+}
+
+func printIDs(sectionName string, matches map[string]string) {
+	fmt.Printf("%v:\n", sectionName)
+	for id, title := range matches {
+		fmt.Printf("[%v] %v\n", id, title)
+	}
+	fmt.Printf("\n\n")
 }
